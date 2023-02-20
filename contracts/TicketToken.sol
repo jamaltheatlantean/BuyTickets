ad12917a0cbe6d8747fd70f1e3c439f1402b8dfc
Gabriel Isobara <jamaltheatlantean@gmail.com>
2023-02-20 07:47:42 +0100

    create NFT

---
+++ contracts/TicketToken.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract TicketToken is ERC721 {
+    uint256 private s_tokenCounter;
+
+    constructor() ERC721("TicketToken", "TKN") {
+        s_tokenCounter = 0;
+    }
+
+    function mintNft() public {
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter = s_tokenCounter + 1;
+    }
+
+    function getTokenCounter() public view returns (uint256) {
+        return s_tokenCounter;
+    }
+}
