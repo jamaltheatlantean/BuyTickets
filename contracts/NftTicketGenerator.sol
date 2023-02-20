0f53d41fd519b9d645efe95b8a6b5412ae26b4f9
Gabriel Isobara <jamaltheatlantean@gmail.com>
2023-02-20 14:56:48 +0100

    update contract

--- contracts/NftTicketGenerator.sol
+++ contracts/NftTicketGenerator.sol
@@ -3,12 +3,18 @@ pragma solidity >=0.8.8 <0.9.0;
 
 // import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
+/**
+ * This contract creates new tickets for users
+ */
+
 contract NftTicketGenerator {
     event TicketBought(address indexed buyer, uint timestamp);
     event TicketMinted(address indexed buyer, uint timestamp);
-    
-    address payable public ticketSeller;
-    address public buyers;
+
+    address payable public ticketSeller; // ticket seller
+    address public buyers; // address of buyers
+
+    uint public constant TICKET_PRICE = 10 * 1e2; // ticket amount
 
 
 }
