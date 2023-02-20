c2d37653fb0a94c2403ee68d0d7c1c8e4643425c
Gabriel Isobara <jamaltheatlantean@gmail.com>
2023-02-20 14:44:23 +0100

    create nftticketgenerator

---
+++ contracts/NftTicketGenerator.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity >=0.8.8 <0.9.0;
+
+// import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract NftTicketGenerator {
+    address payable public ticketSeller;
+    address public buyers;
+}
