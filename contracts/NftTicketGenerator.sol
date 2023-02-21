93f76fad0a36d7eab0d21bc12b8b5b2a0fc568a3
Gabriel Isobara <jamaltheatlantean@gmail.com>
2023-02-21 02:50:19 +0100

    create constructor and function

--- contracts/NftTicketGenerator.sol
+++ contracts/NftTicketGenerator.sol
@@ -7,18 +7,27 @@ pragma solidity >=0.8.8 <0.9.0;
  * This contract creates new tickets for users
  */
 
-contract NftTicketGenerator {
+contract NftTicketGenerator is ERC721 {
     event TicketBought(address indexed buyer, uint timestamp);
     event TicketMinted(address indexed buyer, uint timestamp);
 
     address payable public ticketSeller; // ticket seller
     address public buyers; // address of buyers
 
-    uint public constant TICKET_PRICE = 10 * 1e2; // ticket amount
+    uint public constant TICKET_PRICE = 10 * 1e4; // ticket amount
+    uint public minAmountToPay = 10 * 1e2;
     uint public constant MAX_NUM_OF_TICKET = 100; // only 100 tickets can be minted
 
     mapping(address => bool) public buyer;
     mapping(address => bool) public hasPaid;
     mapping(address => uint) public amountPaid;
+
+    constructor() ERC721("TicketToken", "TKN") {
+        ticketSeller = msg.sender;
+    }
+
+    function buyTicket(uint amount) external payable {
+        require(amount >= minAmountToPay, "error: not enough to pay for ticket");
+    }
 
 }
