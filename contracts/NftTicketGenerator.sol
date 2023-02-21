b45d817da3d938430766200534df14a4e3bc2343
Gabriel Isobara <jamaltheatlantean@gmail.com>
2023-02-21 03:53:03 +0100

    add new variables and complete buyTicket logic

--- contracts/NftTicketGenerator.sol
+++ contracts/NftTicketGenerator.sol
@@ -17,13 +17,14 @@ contract NftTicketGenerator is ERC721 {
     uint public constant TICKET_PRICE = 10 * 1e4; // ticket amount
     uint public minAmountToPay = 10 * 1e2;
 
-    uint public constant MAX_NUM_OF_TICKET = 100; // only 100 tickets can be minted
+    uint public constant MAX_NUM_OF_TICKETS = 100; // only 100 tickets can be minted
     uint public numOfTicketsMinted = 0;
     uint private tokenCounter;
 
     mapping(address => bool) public buyer;
     mapping(address => bool) public hasPaid;
     mapping(address => uint) public amountPaid;
+    mapping(address => bool) public hasBoughtTicket;
 
     constructor() ERC721("TicketToken", "TKN") {
         ticketSeller = msg.sender;
@@ -31,13 +32,21 @@ contract NftTicketGenerator is ERC721 {
 
     function buyTicket(uint amount) external payable {
         require(amount >= minAmountToPay, "error: not enough to pay for ticket");
+        require(hasBoughtTicket[msg.sender] != true, "error: one ticket per wallet");
         hasPaid[msg.sender] = true;
         amountPaid[msg.sender] += msg.value;
-        if(amountPaid[msg.sender] >= TICKET_PRICE && numOfTicketsMinted <= MAX_NUM_OF_TICKET) {
+        if(amountPaid[msg.sender] >= TICKET_PRICE && numOfTicketsMinted <= MAX_NUM_OF_TICKETS) {
             _safeMint(msg.sender, tokenCounter);
             tokenCounter += 1;
             numOfTicketsMinted += 1;
+            hasBoughtTicket[msg.sender] = true;
+            uint ticketFullyPaid = amountPaid[msg.sender];
+            payable(ticketSeller).transfer(ticketFullyPaid);
         }
+    }
+
+    function buyTicketAtOnce() external payable {
+
     }
 
 }
